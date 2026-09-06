3f54571a8a86511bf5d60d2837fd4a23333afea2
danavykhovanets-hub <dana.vykhovanets@gmail.com>
2026-09-06 18:40:30 -0400

    @sidebar + [...filter]

---
+++ app/notes/filter/layout.tsx
@@ -0,0 +1,16 @@
+import css from "./LayoutNotes.module.css";
+import type { ReactNode } from "react";
+
+interface LayoutProps {
+  children: ReactNode;
+  sidebar: ReactNode;
+}
+
+export default function NotesLayout({ children, sidebar }: LayoutProps) {
+  return (
+    <div className={css.container}>
+      <aside className={css.sidebar}>{sidebar}</aside>
+      <main className={css.notesWrapper}>{children}</main>
+    </div>
+  );
+}
