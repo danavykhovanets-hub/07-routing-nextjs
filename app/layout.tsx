c8ee318c90c7011bd750d8255c31d86728bd789e
danavykhovanets-hub <dana.vykhovanets@gmail.com>
2026-09-06 19:57:05 -0400

    Clean up and architecture change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header/Header";
-import TanStackProvider from "./components/TanStackProvider/TanStackProvider";
+import Header from "../components/Header/Header";
+import TanStackProvider from "../components/TanStackProvider/TanStackProvider";
 import { ReactNode } from "react";
 
 const geistSans = Geist({
--- components/NotePreview/NotePreview.tsx
+++ components/NotePreview/NotePreview.tsx
@@ -2,7 +2,7 @@
 
 import { useQuery } from "@tanstack/react-query";
 import { useRouter, useParams } from "next/navigation";
-import { fetchNoteById } from "../../../lib/api";
+import { fetchNoteById } from "../../lib/api";
 import Modal from "../Modal/Modal";
 import css from "./NotePreview.module.css";
 
